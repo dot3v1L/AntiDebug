7be3c57244fc4442e8e8db682ee24502a582b2b3
dot3v1L <cryptonly4you@gmail.com>
2017-07-22 14:28:51 +0200

    AntiDebug
    Добавление AntiDebug при помощи Mono.Cecil

---
+++ AntiDebug/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace AntiDebug
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.fOpen = new System.Windows.Forms.Button();
+            this.fSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // fOpen
+            // 
+            this.fOpen.Location = new System.Drawing.Point(168, 10);
+            this.fOpen.Name = "fOpen";
+            this.fOpen.Size = new System.Drawing.Size(75, 23);
+            this.fOpen.TabIndex = 1;
+            this.fOpen.Text = "File";
+            this.fOpen.UseVisualStyleBackColor = true;
+            this.fOpen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fSave
+            // 
+            this.fSave.Location = new System.Drawing.Point(12, 38);
+            this.fSave.Name = "fSave";
+            this.fSave.Size = new System.Drawing.Size(231, 23);
+            this.fSave.TabIndex = 2;
+            this.fSave.Text = "Bummmmmp";
+            this.fSave.UseVisualStyleBackColor = true;
+            this.fSave.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 72);
+            this.Controls.Add(this.fSave);
+            this.Controls.Add(this.fOpen);
+            this.Controls.Add(this.textBox1);
+            this.Name = "fMain";
+            this.Text = "Anti-Debug";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button fOpen;
+        private System.Windows.Forms.Button fSave;
+    }
+}
+
---
+++ AntiDebug/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace AntiDebug
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var open = new OpenFileDialog())
+            {
+                open.Filter = ".exe|*.exe";
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = open.FileName;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var save = new SaveFileDialog())
+            {
+                save.Filter = ".exe|*.exe";
+                save.FileName = String.Format("{0}_anti", Path.GetFileNameWithoutExtension(textBox1.Text));
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    AntiDebag(textBox1.Text,save.FileName);
+                    MessageBox.Show("Oopss");
+                }
+            }
+        }
+
+        private void AntiDebag(string path, string outPath)
+        {
+            AssemblyDefinition assembly = AssemblyDefinition.ReadAssembly(path);
+            TypeDefinition t = assembly.MainModule.GetType("<Module>");
+            MethodDefinition cctor = null; // <Module>.cctor
+            foreach (MethodDefinition m in t.Methods)
+            {
+                if (m.Name == ".cctor" && m.IsConstructor)
+                {
+                    cctor = m;
+                    break;
+                }
+            }
+            if (cctor == null) // если ни один cctor не найден, создать новый
+            {
+                cctor = new MethodDefinition(
+                    ".cctor",
+                    Mono.Cecil.MethodAttributes.Private |
+                    Mono.Cecil.MethodAttributes.Static |
+                    Mono.Cecil.MethodAttributes.HideBySig |
+                    Mono.Cecil.MethodAttributes.SpecialName |
+                    Mono.Cecil.MethodAttributes.RTSpecialName
+                    , assembly.MainModule.Import(typeof(void)));
+                t.Methods.Add(cctor);
+            }
+            if (cctor.Body.Instructions.Count > 0 && cctor.Body.Instructions.Last<Instruction>().OpCode == OpCodes.Ret)
+            {
+                cctor.Body.Instructions.Remove(cctor.Body.Instructions.Last<Instruction>()); // если cctor уже существовал удалить последний
+            }
+            MethodDefinition AntiDebug = new MethodDefinition(// Метод antidebug (управляемый), который будет выполнен в новом потоке
+                "AntiDebug",
+                Mono.Cecil.MethodAttributes.Static |
+                Mono.Cecil.MethodAttributes.Private,
+                assembly.MainModule.Import(typeof(void)));
+            
+            ILProcessor CtorProcessor = cctor.Body.GetILProcessor();
+            CtorProcessor.Append(CtorProcessor.Create(OpCodes.Ldnull));
+            CtorProcessor.Append(CtorProcessor.Create(OpCodes.Ldftn, AntiDebug));
+            CtorProcessor.Append(CtorProcessor.Create(OpCodes.Newobj,
+                assembly.MainModule.Import(typeof(ThreadStart).GetConstructor(new Type[] { typeof(object), typeof(IntPtr) })))); // если указано несколько методов с тем же именем Тип  параметр 
+            CtorProcessor.Append(CtorProcessor.Create(OpCodes.Newobj, assembly.MainModule.Import(typeof(Thread).GetConstructor(new Type[] { typeof(ThreadStart) }))));
+            CtorProcessor.Append(CtorProcessor.Create(OpCodes.Call, assembly.MainModule.Import(typeof(Thread).GetMethod("Start", Type.EmptyTypes))));
+            CtorProcessor.Append(CtorProcessor.Create(OpCodes.Ret));
+
+            ILProcessor AntiProcessor = AntiDebug.Body.GetILProcessor();
+            Instruction First = AntiProcessor.Create(OpCodes.Call, assembly.MainModule.Import(typeof(Debugger).GetMethod("get_IsAttached")));
+            Instruction Exit = AntiProcessor.Create(OpCodes.Ldc_I4_0);
+            AntiProcessor.Append(First);
+            AntiProcessor.Append(AntiProcessor.Create(OpCodes.Brtrue_S, Exit));
+            AntiProcessor.Append(AntiProcessor.Create(OpCodes.Call, assembly.MainModule.Import(typeof(Debugger).GetMethod("IsLogging"))));
+            AntiProcessor.Append(AntiProcessor.Create(OpCodes.Brtrue_S, Exit));
+            AntiProcessor.Append(AntiProcessor.Create(OpCodes.Ldc_I4, 1000));
+            AntiProcessor.Append(AntiProcessor.Create(OpCodes.Call, assembly.MainModule.Import(typeof(Thread).GetMethod("Sleep", new Type[] { typeof(Int32) }))));
+            AntiProcessor.Append(AntiProcessor.Create(OpCodes.Br_S, First));
+            AntiProcessor.Append(Exit);
+            AntiProcessor.Append(AntiProcessor.Create(OpCodes.Call, assembly.MainModule.Import(typeof(Environment).GetMethod("Exit", new Type[] { typeof(int) }))));
+            AntiProcessor.Append(AntiProcessor.Create(OpCodes.Ret));
+            t.Methods.Add(AntiDebug);
+            assembly.Write(outPath);
+        }
+    }
+}
